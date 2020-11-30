6af64cfc6214d1f6154da743c844cd1eb00bc52a
MichalMal <michal@mobilemacs.co.za>
2020-11-30 05:32:21 +0200

    testing out the final changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/videos'
-  get 'static_pages/bus_tours'
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/videos', to: 'static_pages#videos'
+  get '/bus_tours', to: 'static_pages#bus_tours'
   root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
